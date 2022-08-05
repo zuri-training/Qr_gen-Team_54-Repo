7c53b9c80d06e2153cb77daf094593a3593c4902
Robsen Gelana <robsengelana21@gmail.com>
2022-08-05 11:21:46 +0300

    adding a view fie on the website app

--- apps/websites/views.py
+++ apps/websites/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-
+from .models import Website
 
 
 def generate_qr_code(request):
-    pass
+    if request.method == "POST":
+        Url = request.POST['url']
+        Title = request.POST['title']
+        Website.objects.create(url=Url, name=Title)
+
+    qr_code = Website.objects.all()
+    return render(request, "index.html", {'qr_code': qr_code})
