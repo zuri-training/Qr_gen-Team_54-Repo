81d335dd04021912e4d6b9948e4dc65586006f59
MJspitta <madu.jang93@gmail.com>
2022-08-07 13:19:06 +0100

    media, faq, contact page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,8 @@ const toggler = document.querySelector("#toggler")
 const toggle_Menu = document.querySelector(".nav-group-mobile")
 const signUp = document.querySelector(".mob-active")
 const logIn = document.querySelector(".mob-link")
+const holder = document.querySelector('.header-username')
+const navDesk = document.querySelector('.nav-desktop')
 
 
 toggler.addEventListener("click", () => {
@@ -12,4 +14,12 @@ toggler.addEventListener("click", () => {
 document.querySelectorAll(".mob-link").forEach(n => n.addEventListener("click",()=> {
     toggler.classList.remove("activated")
     toggle_Menu.classList.remove("activatede")
+}))
+
+holder.addEventListener('click',()=> {
+    navDesk.classList.toggle('nav-active')
+})
+
+document.querySelector(".link").forEach(n => n.addEventListener("click", () => {
+    navDesk.classList.remove('nav-active')
 }))
