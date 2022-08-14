c97f1f84d9bfc1e34f67a1576d8a53ae51e8e6e3
olowu abayomi <aayobam@gmail.com>
2022-08-14 13:57:17 +0100

    added pdf download feature and fixed django heroku issue

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = [
+ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     'qrx-gen.herokuapp.com',
